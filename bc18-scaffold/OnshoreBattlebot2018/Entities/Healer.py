b87848074ea6702b1dcd9b15f3b8678324965c29
enriquemoeller <enrique.moeller@onshoreoutsourcing.com>
2018-01-10 21:58:59 -0600

    Healer stubbed

--- bc18-scaffold/OnshoreBattlebot2018/Entities/Healer.py
+++ bc18-scaffold/OnshoreBattlebot2018/Entities/Healer.py
@@ -12,3 +12,27 @@ class Healer(IRobot):
 
 	def run(self):
 		pass
+
+	def tryHeal(self, targetRobotId):
+		#TODO check heat is low enough
+		if not self.gameController.can_heal(self.robotId, targetRobotId):
+			print ("Healer [{}] cannot heal the target [{}]".format(self.robotId, targetRobotId))
+			return False
+
+		self.gameController.heal(self.robotId, targetRobotId)
+		return True
+
+	def tryOvercharge(self, targetRobotId):
+		#TODO check has research
+		
+		#Check cooldown of Overcharge ability
+		if not self.gameController.is_overcharge_ready(self.robotId):
+			print("Overcharge is not ready for Healer [{}]".format(self.robotId))
+			return False
+
+		if not self.gameController.can_overcharge(self.robotId, targetRobotId):
+			print("Healer [{}] cannot Overcharge the target [{}]".format(self.robotId, targetRobotId))
+			return False
+
+		self.gameController.overcharge(self.robotId, targetRobotId)
+		return True
