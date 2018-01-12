3197a6f47945bd73d88a88d835361cc1c012f62d
Ryan Legow <35343172+soulcaliburedge@users.noreply.github.com>
2018-01-12 16:49:33 -0600

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-set PYTHONPATH=..\battlecode\python
-py -3 run.py
+docker pull battlecode/battlecode-2018
+
+docker run -it --privileged -p 16147:16147 -p 6147:6147 -v "%~dp0:/player" "battlecode/battlecode-2018"
