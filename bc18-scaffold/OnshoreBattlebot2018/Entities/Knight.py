0dc076a61097ee57b2541a70d0d84db4c652d806
DarkFortress2\Elyk Yevrah <elykyevrah@gmail.com>
2018-01-12 07:01:40 -0600

    Stubbed Knight with simple get mission from unit controller.

--- bc18-scaffold/OnshoreBattlebot2018/Entities/Knight.py
+++ bc18-scaffold/OnshoreBattlebot2018/Entities/Knight.py
@@ -9,8 +9,30 @@ class Knight(IRobot):
 	# For example:  it will eventually need to access the Targeting and Pathfinding controllers
 	def __init__(self, gameController, unitController, pathfindingController, unit):
 		super().__init__(gameController, unitController, pathfindingController, unit)
+		self.mission = None
+		self.targetLocation = None
+		self.path = None
+
 
 	def run(self):
+		self.__GetMission()
+
+		if self.mission == "Walk Randomly":
+			print("walking randomly")
+			#TODO rework this section to include pathfinding as well as path traversal.
+				
+
+	def __GetMission(self):
+		if self.mission == None:
+			print("Knight has no mission.")
+			self.mission = self.unitController.GetMission(self.unit.unit_type)
+			print("New mission is ({})".format(self.mission))
+		else:
+			print("Current mission is ({})".format(self.mission))
+
+	def IsEnemySighted(self):
+		#TODO determine if this unit is able to see an enemy.
+		#TODO refactor code to IRobot so that all robots can sight enemies.
 		pass
 
 	def tryAttack(self, targetRobotId):
