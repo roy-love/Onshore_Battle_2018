4613267c669e33153097fb790b4c802f807f264b
DarkFortress2\Elyk Yevrah <elykyevrah@gmail.com>
2018-01-13 12:16:59 -0600

    Created Missions Controller and logic to get and add missions

---
+++ bc18-scaffold/OnshoreBattlebot2018/Controllers/MissionController.py
@@ -0,0 +1,89 @@
+import random
+import sys
+import traceback
+
+import battlecode as bc
+from .StrategyController import *
+
+class Missions(enum):
+    Idle = 0
+    RandomMovement = 1
+    Mining = 2
+    FollowUnit = 3
+    Scout = 4
+    Patrol = 5
+    DestroyTarget = 6
+    DefendTarget = 7
+    
+# Controller that handles the creation and managment of missions
+class MissionController:
+    def __init__(self, gameController, strategyController):
+	    self.gameController = gameController
+	    self.strategyController = strategyController
+
+        self.combatMissions = []
+        self.healerMissions = []
+        self.workerMissions = []
+
+    # Adds a new mission created by outside source
+    def AddMission(self,unitType,mission):
+        if unitType == bc.UnitType.Worker:
+            self.workerMissions.append(mission)
+        elif unitType == bc.UnitType.Healer:
+            self.healerMissions.append(mission)
+        else:
+            self.combatMissions.append(mission)
+
+    #Pops the highest priority mission for the given unit off the queue and returns it
+    def GetMission(self,unit):
+        # Return a mission based on the type of unit
+        # The mission will be based on the current strategy
+        
+        if unit.unit_type == bc.UnitType.Worker:
+            if len(self.workerMissions) > 0:
+                print("Worker mission assigned")
+                return self.workerMissions.pop(0)
+            else
+                return __CreateNewWorkerMission__(self)
+        elif unit.unit_type == bc.UnitType.Healer:
+            if len(self.healerMissions) > 0:
+                print("Healer mission assigned")
+                __CreateNewHealerMission__(self)
+            else
+                return self.healerMissions.pop(0)
+        else:
+            if len(self.combatMissions) > 0:
+                print("Combat mission assigned")
+                return self.combatMissions.pop(0)
+
+    def __CreateNewWorkerMission__(self):
+        #Determine what mission to assign based on the current strategy
+        if self.strategyController.unitStrategy == self.strategyController.UnitStrategies.Default
+            chance = random.randint(0,100)
+		    if chance > 50:    
+                return Missions.RandomMovement
+            elif change > 25:
+                return Missions.Mining
+            else:
+                return Missions.Idle
+    
+    def __CreateNewHealerMission__(self):
+
+        if self.strategyController.unitStrategy == self.strategyController.UnitStrategies.Default:
+            chance = random.randint(0,100)
+            if chance > 50:
+                return Missions.FollowUnit
+            else:
+                return Missions.Idle
+    
+    def __CreateNewCombatMission__(self):
+
+        if self.strategyController.unitStrategy == self.strategyController.UnitStrategies.Default:
+            chance = random.randint(0,100)
+            if chance > 50:
+                return Missions.Scout
+            elif chance > 25:
+                return Missions.Patrol
+            else:
+                return Missions.Idle
+
